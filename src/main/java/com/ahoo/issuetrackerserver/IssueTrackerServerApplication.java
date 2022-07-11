5cea08e9c013d1cad5cf97fd33f639188df7c553
WooJin Park(Ader) <ak2j38@gmail.com>
2022-07-11 14:22:57 +0900

    🎉 프로젝트 최초 셋팅
    Co-authored-by: who-hoo <who.ho3ov@gmail.com>

---
+++ src/main/java/com/ahoo/issuetrackerserver/IssueTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.ahoo.issuetrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueTrackerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahoo/issuetrackerserver/IssueTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahoo.issuetrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
