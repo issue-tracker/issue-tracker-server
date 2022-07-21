3c9b48a4c91b35cba020068e55181b4e39d6e583
who-hoo <who.ho3ov@gmail.com>
2022-07-21 16:59:44 +0900

    ✅ 일반회원가입 테스트 코드 작성
    Co-Authored-By: WooJin Park(Ader) <ak2j38@gmail.com>

--- src/main/java/com/ahoo/issuetrackerserver/member/Member.java
+++ src/main/java/com/ahoo/issuetrackerserver/member/Member.java
@@ -9,13 +9,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(staticName = "of")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(exclude = {"loginId", "password", "email", "nickname", "profileImage"})
 public class Member {
 
     @Id
@@ -40,4 +42,14 @@ public class Member {
     @Column(nullable = false, updatable = false)
     @Enumerated(EnumType.STRING)
     private AuthProvider authProviderType;
+
+	public static Member of(Long id, String loginId, String password, String email, String nickname,
+		String profileImage, AuthProvider authProviderType) {
+		return new Member(id, loginId, password, email, nickname, profileImage, authProviderType);
+	}
+
+	public static Member of(String loginId, String password, String email, String nickname,
+		String profileImage, AuthProvider authProviderType) {
+		return new Member(null, loginId, password, email, nickname, profileImage, authProviderType);
+	}
 }
--- src/main/java/com/ahoo/issuetrackerserver/member/dto/GeneralMemberCreateRequest.java
+++ src/main/java/com/ahoo/issuetrackerserver/member/dto/GeneralMemberCreateRequest.java
@@ -3,11 +3,15 @@ package com.ahoo.issuetrackerserver.member.dto;
 import com.ahoo.issuetrackerserver.member.AuthProvider;
 import com.ahoo.issuetrackerserver.member.Member;
 import java.util.Optional;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GeneralMemberCreateRequest {
 
     private String loginId;
@@ -25,5 +29,9 @@ public class GeneralMemberCreateRequest {
             this.nickname,
             Optional.ofNullable(this.profileImage).orElse("defaultS3ImageUrl"),
             AuthProvider.NONE);
+    }
+
+    public static GeneralMemberCreateRequest of(String loginId, String password, String email, String nickname, String profileImage) {
+        return new GeneralMemberCreateRequest(loginId, password, email, nickname, Optional.ofNullable(profileImage).orElse("defaultS3ImageUrl"));
     }
 }
