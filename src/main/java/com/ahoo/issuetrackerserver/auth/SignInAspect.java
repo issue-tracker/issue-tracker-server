53eb67af36a5399af76d891a1137e2dc26574db9
WooJin Park(Ader) <ak2j38@gmail.com>
2022-07-30 01:20:52 +0900

    🩹 AOP 적용시점을 메소드 전으로 변경
    Co-Authored-By: who-hoo <68011320+who-hoo@users.noreply.github.com>

--- src/main/java/com/ahoo/issuetrackerserver/auth/SignInAspect.java
+++ src/main/java/com/ahoo/issuetrackerserver/auth/SignInAspect.java
@@ -7,9 +7,8 @@ import java.util.Optional;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -20,14 +19,12 @@ public class SignInAspect {
     private final JwtService jwtService;
     private final HttpServletRequest httpServletRequest;
 
-    //TODO : 메소드 네이밍 리팩토링
-    @Around("@annotation(com.ahoo.issuetrackerserver.auth.SignInRequired)")
-    public Object accessToken(final ProceedingJoinPoint pjp) throws Throwable {
+    @Before("@annotation(com.ahoo.issuetrackerserver.auth.SignInRequired)")
+    public void validateAccessToken() {
         try {
             AccessToken accessToken = extractAccessToken().orElseThrow(
                 () -> new UnAuthorizedException("요청에 access_token 쿠키가 존재하지 않습니다."));
             jwtService.validateAccessToken(accessToken);
-            return pjp.proceed();
         } catch (JwtException e) {
             throw new UnAuthorizedException(e.getMessage(), e);
         }
