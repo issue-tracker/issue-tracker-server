50350ffae4501bb75f670ebe31d6a8d7949f8b95
WooJin Park(Ader) <ak2j38@gmail.com>
2022-08-05 01:16:37 +0900

    ✅ 일반 로그인 테스트 코드 작성

--- src/test/java/com/ahoo/issuetrackerserver/member/MemberServiceTest.java
+++ src/test/java/com/ahoo/issuetrackerserver/member/MemberServiceTest.java
@@ -10,6 +10,7 @@ import com.ahoo.issuetrackerserver.auth.AuthProvider;
 import com.ahoo.issuetrackerserver.exception.DuplicateMemberException;
 import com.ahoo.issuetrackerserver.member.dto.AuthMemberCreateRequest;
 import com.ahoo.issuetrackerserver.member.dto.GeneralMemberCreateRequest;
+import com.ahoo.issuetrackerserver.member.dto.GeneralSignInRequest;
 import com.ahoo.issuetrackerserver.member.dto.MemberResponse;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -19,7 +20,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
@@ -33,15 +33,6 @@ class MemberServiceTest {
     @Nested
     @DisplayName("일반 회원가입은")
     class Describe_signUpByGeneral {
-
-        @BeforeTestClass
-        void beforeTestClass() {
-            Member member = Member.of(1L, "who-hoo", "1234", "who.ho3ov@gmail.com", "hoo",
-                "https://avatars.githubusercontent.com/u/68011320?v=4", AuthProvider.GITHUB, "68011320");
-            given(memberRepository.findByEmail("who.ho3ov@gmail.com")).willReturn(Optional.of(member));
-            given(memberRepository.existsByLoginId("who-hoo")).willReturn(true);
-            given(memberRepository.existsByNickname("hoo")).willReturn(true);
-        }
 
         @Test
         void 중복되지_않는_이메일_닉네임_아이디를_가진_회원가입_요청_입력이_주어지면_회원가입이_성공한다() {
@@ -136,15 +127,6 @@ class MemberServiceTest {
     @DisplayName("Auth 회원가입은")
     class Describe_signUpByAuth {
 
-        @BeforeTestClass
-        void beforeTestClass() {
-            Member member = Member.of(1L, "who-hoo", "1234", "who.ho3ov@gmail.com", "hoo",
-                "https://avatars.githubusercontent.com/u/68011320?v=4", AuthProvider.GITHUB, "68011320");
-            given(memberRepository.findByEmail("who.ho3ov@gmail.com")).willReturn(Optional.of(member));
-            given(memberRepository.existsByLoginId("who-hoo")).willReturn(true);
-            given(memberRepository.existsByNickname("hoo")).willReturn(true);
-        }
-
         @Test
         void 중복되지_않는_이메일_닉네임을_가진_회원가입_요청_입력이_주어지면_회원가입이_성공한다() {
             //given
@@ -203,6 +185,63 @@ class MemberServiceTest {
                 .hasMessage("중복되는 닉네임이 존재합니다.");
             then(memberRepository).should(times(1)).findByEmail(failureRequest.getEmail());
             then(memberRepository).should(times(1)).existsByNickname(failureRequest.getNickname());
+            then(memberRepository).shouldHaveNoMoreInteractions();
+        }
+    }
+
+    @Nested
+    @DisplayName("일반 로그인은")
+    class Describe_signInByGeneral {
+
+        @Test
+        void 존재하는_아이디와_비밀번호로_로그인_요청_입력이_주어지면_로그인이_성공한다() {
+            // given
+            Member member = Member.of(1L, "who-hoo", "1234", "who.ho3ov@gmail.com", "hoo",
+                "https://avatars.githubusercontent.com/u/68011320?v=4", AuthProvider.GITHUB, "68011320");
+            given(memberRepository.findByLoginId("who-hoo")).willReturn(Optional.of(member));
+            GeneralSignInRequest generalSignInRequest = GeneralSignInRequest.of("who-hoo", "1234");
+
+            // when
+            MemberResponse memberResponse = memberService.signInByGeneral(generalSignInRequest.getId(),
+                generalSignInRequest.getPassword());
+
+            // then
+            then(memberRepository).should(times(1)).findByLoginId(generalSignInRequest.getId());
+            then(memberRepository).shouldHaveNoMoreInteractions();
+            assertThat(memberResponse.getId()).isEqualTo(1L);
+        }
+
+        @Test
+        void 존재하지_않는_아이디로_로그인_요청_입력이_주어지면_IllegalArgumentException_예외가_발생하며_로그인이_실패한다() {
+            // given
+            GeneralSignInRequest generalSignInRequest = GeneralSignInRequest.of("ader", "1234");
+            given(memberRepository.findByLoginId(generalSignInRequest.getId())).willReturn(Optional.empty());
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> memberService.signInByGeneral(generalSignInRequest.getId(), generalSignInRequest.getPassword()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로그인에 실패했습니다. 아이디와 비밀번호를 다시 확인해주세요.");
+            then(memberRepository).should(times(1)).findByLoginId(generalSignInRequest.getId());
+            then(memberRepository).shouldHaveNoMoreInteractions();
+        }
+
+        @Test
+        void 일치하지_않는_비밀번호로_로그인_요청_입력이_주어지면_IllegalArgumentException_예외가_발생하며_로그인이_실패한다() {
+            // given
+            Member member = Member.of(1L, "who-hoo", "1234", "who.ho3ov@gmail.com", "hoo",
+                "https://avatars.githubusercontent.com/u/68011320?v=4", AuthProvider.GITHUB, "68011320");
+            given(memberRepository.findByLoginId("who-hoo")).willReturn(Optional.of(member));
+            GeneralSignInRequest generalSignInRequest = GeneralSignInRequest.of("who-hoo", "4321");
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> memberService.signInByGeneral(generalSignInRequest.getId(), generalSignInRequest.getPassword()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로그인에 실패했습니다. 아이디와 비밀번호를 다시 확인해주세요.");
+            then(memberRepository).should(times(1)).findByLoginId(generalSignInRequest.getId());
             then(memberRepository).shouldHaveNoMoreInteractions();
         }
     }
