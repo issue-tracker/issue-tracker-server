e8236a34136c6dcb933fb5cfedf01439b674236b
who-hoo <who.ho3ov@gmail.com>
2022-08-15 22:33:38 +0900

    🤡 마일스톤 더미 데이터 추가(닫힌 이슈)

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -4,4 +4,5 @@ VALUES ('who-hoo', '1234', 'who.ho3ov@gmail.com', 'hoo', 'https://avatars.github
 INSERT INTO milestone (title, description, due_date, is_closed)
 VALUES ('제목만 있는 마일스톤', null, null, false),
        ('제목과 설명이 있는 마일스톤', '하지만 완료일은 없다', null, false),
-       ('모든걸 다 가진 마일스톤', 'perfect', '2022-08-31', false);
+       ('모든걸 다 가진 마일스톤', 'perfect', '2022-08-31', false),
+       ('닫혀버린 마일스톤', 'closed', '2022-08-31', true);
