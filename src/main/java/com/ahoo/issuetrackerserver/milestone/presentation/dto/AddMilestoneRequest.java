e062af9b1b77756eccf88af7810a1595f7ffd26a
who-hoo <who.ho3ov@gmail.com>
2022-08-16 14:52:14 +0900

    📝 Swagger API 문서화를 위한 어노테이션 추가

--- src/main/java/com/ahoo/issuetrackerserver/milestone/presentation/dto/AddMilestoneRequest.java
+++ src/main/java/com/ahoo/issuetrackerserver/milestone/presentation/dto/AddMilestoneRequest.java
@@ -9,18 +9,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+@Schema(description = "마일스톤 추가 요청")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AddMilestoneRequest {
 
-	@Schema(required = true)
+	@Schema(required = true, description = "마일스톤 이름")
 	@NotBlank
 	@Length(max = 255)
 	private String title;
 
+	@Schema(description = "마일스톤 설명(선택)")
 	@Length(max = 255)
 	private String description;
 
+	@Schema(description = "마일스톤 완료일(선택) ex)YYYY-MM-DD")
 	private LocalDate dueDate;
 
 	public Milestone toEntity() {
--- src/main/java/com/ahoo/issuetrackerserver/milestone/presentation/dto/UpdateMilestoneRequest.java
+++ src/main/java/com/ahoo/issuetrackerserver/milestone/presentation/dto/UpdateMilestoneRequest.java
@@ -8,17 +8,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+@Schema(description = "마일스톤 수정 요청")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UpdateMilestoneRequest {
 
-	@Schema(required = true)
+	@Schema(required = true, description = "마일스톤 이름")
 	@NotBlank
 	@Length(max = 255)
 	private String title;
 
+	@Schema(description = "마일스톤 설명(선택)")
 	@Length(max = 255)
 	private String description;
 
+	@Schema(description = "마일스톤 완료일(선택) ex)YYYY-MM-DD")
 	private LocalDate dueDate;
 }
