ae5798fed31e82934d1cff1f3dfb3e5b1b6ff4df
who-hoo <who.ho3ov@gmail.com>
2022-08-23 02:50:46 +0900

    ⚡️ ToOne 연관관계를 가진 엔티티 fetch join하도록 추가
    Co-Authored-By: WooJin Park(Ader) <ak2j38@gmail.com>

--- src/main/java/com/ahoo/issuetrackerserver/issue/infrastructure/IssueRepository.java
+++ src/main/java/com/ahoo/issuetrackerserver/issue/infrastructure/IssueRepository.java
@@ -11,22 +11,32 @@ import org.springframework.data.repository.query.Param;
 public interface IssueRepository extends JpaRepository<Issue, Long> {
 
     @Query("select distinct i "
-        + "from Issue i left join fetch i.comments "
+        + "from Issue i "
+        + "join fetch i.author "
+        + "left join fetch i.milestone "
+        + "left join fetch i.comments "
         + "where i.id = :id")
     Optional<Issue> findByIdFetchJoinComments(@Param("id") Long id);
 
     @Query("select distinct i "
-        + "from Issue i left join fetch i.assignees "
+        + "from Issue i "
+        + "join fetch i.author "
+        + "left join fetch i.milestone "
+        + "left join fetch i.assignees "
         + "where i.id = :id")
     Optional<Issue> findByIdFetchJoinAssignees(@Param("id") Long id);
 
     @Query("select distinct i "
-        + "from Issue i left join fetch i.labels "
+        + "from Issue i "
+        + "join fetch i.author "
+        + "left join fetch i.milestone "
+        + "left join fetch i.labels "
         + "where i.id = :id")
     Optional<Issue> findByIdFetchJoinLabels(@Param("id") Long id);
 
     @Query("select distinct i "
-        + "from Issue i left join fetch i.milestone "
+        + "from Issue i "
+        + "left join fetch i.milestone "
         + "where i.id = :id")
     Optional<Issue> findByIdFetchJoinMilestone(@Param("id") Long id);
 
