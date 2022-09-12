d15515329fdeeb3490fa431cfaab24deabfac011
WooJin Park(Ader) <ak2j38@gmail.com>
2022-09-12 12:56:07 +0900

    ✨ 첫 번째 코멘트는 삭제할 수 없다는 예외 타입 추가
    Co-Authored-By: who-hoo <68011320+who-hoo@users.noreply.github.com>

--- src/main/java/com/ahoo/issuetrackerserver/common/exception/ErrorType.java
+++ src/main/java/com/ahoo/issuetrackerserver/common/exception/ErrorType.java
@@ -36,6 +36,7 @@ public enum ErrorType {
     NOT_EXISTS_MILESTONE(HttpStatus.BAD_REQUEST, "존재하지 않는 마일스톤입니다."),
     NOT_EXISTS_REACTION(HttpStatus.BAD_REQUEST, "존재하지 않는 리액션입니다."),
 
+    NOT_DELETABLE_COMMENT(HttpStatus.BAD_REQUEST, "삭제할 수 없는 코멘트입니다."),
     NOT_MATCHED_MILESTONE(HttpStatus.BAD_REQUEST, "삭제하려는 마일스톤이 해당 이슈에 존재하지 않습니다.");
 
     private final HttpStatus status;
