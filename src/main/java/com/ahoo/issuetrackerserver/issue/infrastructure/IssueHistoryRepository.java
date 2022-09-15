3474a25a35038a5b5cf78f104f5d262d0a075e43
WooJin Park(Ader) <ak2j38@gmail.com>
2022-09-16 00:00:33 +0900

    🐛 이슈 삭제 시 이슈히스토리 남기는 로직에서 에러 나는 현상 개선

--- src/main/java/com/ahoo/issuetrackerserver/issue/infrastructure/IssueHistoryRepository.java
+++ src/main/java/com/ahoo/issuetrackerserver/issue/infrastructure/IssueHistoryRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IssueHistoryRepository extends JpaRepository<IssueHistory, Long> {
 
+    void deleteAllByIssueId(Long issueId);
+
 }
